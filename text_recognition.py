373025802080d073b3159274519202642dafcaf0
Gergo Dobi <dobigergo96@gmail.com>
2020-08-18 12:51:00 +0200

    project files created

--- text_recognition.py
+++ text_recognition.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import pytesseract
 import falcon
+import json
 
 def filter_empty_strings(word):
     empty = ['',' ', '/n']
@@ -10,22 +11,47 @@ def filter_empty_strings(word):
         return False
     else:
         return True
+        
+class TranslationEntries:
+    
+    word1 = ""
+    word2 = ""
+    
+    __init__(self,word1,word2):
+        self.word1 = word1
+        self.word2 = word2
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-img = cv2.imread("orvosi-tablazat-1.jpg")
+def make_json(wordList1, wordList2):
+    
+    translationList = []
+    for i in range(len(wordList1)):
+        translationList.add(TranslationEntries(wordList1[i],wordList2[i]))
+        
+        
+    return json.dumps(translationList)
+    
+class Response:
 
-length, width, dim = np.shape(img)
+    def on_get(self,req,resp):
+        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-img_hun = img[0:length, 0:width//2]
-img_eng = img[0:length, width//2:width]
+        img = cv2.imread("orvosi-tablazat-1.jpg")
 
-text_eng = pytesseract.image_to_string(img_eng)
-text_hun = pytesseract.image_to_string(img_hun, lang = "hun")
+        length, width, dim = np.shape(img)
 
-text_hun_set = list(filter(filter_empty_strings,text_hun.split("\n")))
-text_eng_set = list(filter(filter_empty_strings,text_eng.split("\n")))
+        img_hun = img[0:length, 0:width//2]
+        img_eng = img[0:length, width//2:width]
 
-print(text_hun_set)
-print(text_eng_set)
+        text_eng = pytesseract.image_to_string(img_eng)
+        text_hun = pytesseract.image_to_string(img_hun, lang = "hun")
 
+        text_hun_set = list(filter(filter_empty_strings,text_hun.split("\n")))
+        text_eng_set = list(filter(filter_empty_strings,text_eng.split("\n")))
+        
+        resp.body = make_json(text_hun_set,text_eng_set)
+
+       
+api = falcon.API()
+response = response()
+api.add_route('/', response)
