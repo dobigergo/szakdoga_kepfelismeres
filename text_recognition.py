3efcd1dc9b2a297004a78eeff487ae9d9e76b921
Gergo Dobi <dobigergo96@gmail.com>
2020-08-21 17:47:32 +0200

    falcon replaced with flask

--- text_recognition.py
+++ text_recognition.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
 import pytesseract
-import falcon
+import flask
 import json
+
+
+app = Flask(__name__)
 
 def filter_empty_strings(word):
     empty = ['',' ', '/n']
@@ -24,28 +27,19 @@ def make_json(wordList1, wordList2):
             
     return json.dumps(translationList, ensure_ascii=False).encode('utf8')
     
-class Response:
-
-    def on_get(self,req,resp):
+@app.route('/')
+def on_get():
         
-        pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract' 
-        
-        img = cv2.imread('orvosi-tablazat-1.jpg')
+    pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 
-        length, width, dim = np.shape(img)
+    img = cv2.imread('orvosi-tablazat-1.jpg')
 
-        img_hun = img[0:length, 0:width//2]
-        img_eng = img[0:length, width//2:width]
-
-        text_eng = pytesseract.image_to_string(img_eng)
-        text_hun = pytesseract.image_to_string(img_hun, lang = "hun")
-
-        text_hun_set = list(filter(filter_empty_strings,text_hun.split("\n")))
-        text_eng_set = list(filter(filter_empty_strings,text_eng.split("\n")))
-        
-        resp.body = make_json(text_hun_set,text_eng_set)
-
-       
-api = falcon.API()
-response = Response()
-api.add_route('/', response)
+    length, width, dim = np.shape(img)
+    img_hun = img[0:length, 0:width//2]
+    img_eng = img[0:length, width//2:width]
+    text_eng = pytesseract.image_to_string(img_eng
+    text_hun = pytesseract.image_to_string(img_hun, lang = "hun")
+    text_hun_set = list(filter(filter_empty_strings,text_hun.split("\n"))
+    text_eng_set = list(filter(filter_empty_strings,text_eng.split("\n")))
+    
+    return make_json(text_hun_set,text_eng_set)
