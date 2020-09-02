d380ef85d5dcd4beb1ba70598b5a9865caf562ab
Gergo Dobi <dobigergo96@gmail.com>
2020-09-02 21:47:27 +0200

    POST methode changed, deploy to test with the mobile app

--- text_recognition.py
+++ text_recognition.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import pytesseract
+import base64
 from flask import Flask
 from flask import request
 import json
@@ -10,7 +11,6 @@ app = Flask(__name__)
 #production path: '/app/.apt/usr/bin/tesseract'
 #development path: 'C:\Program Files\Tesseract-OCR\tesseract.exe'
 pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
-
 
 def filter_empty_strings(word):
     empty = ['',' ', '/n']
@@ -52,8 +52,11 @@ def on_get():
 @app.route('/img_process', methods = ["POST"])
 def on_post():
 
-    img = cv2.imread('orvosi-tablazat-1.jpg')
-    
+    request_dict = json.loads(request.json)
+    im_bytes = base64.b64decode(request_dict["data"])
+    im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
+    img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+
     length, width, dim = np.shape(img)
     img_hun = img[0:length, 0:width//2]
     img_eng = img[0:length, width//2:width]
@@ -61,9 +64,9 @@ def on_post():
     text_hun = pytesseract.image_to_string(img_hun, lang = "hun")
     text_hun_set = list(filter(filter_empty_strings,text_hun.split("\n")))
     text_eng_set = list(filter(filter_empty_strings,text_eng.split("\n")))
-
     
     return make_json(text_hun_set,text_eng_set)
+ 
     
     
 
